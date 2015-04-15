13369d6eafda666d768e60eb133d5f1ba76a5471
SruthiSolipuram <solipuram_sr@yahoo.com>
2015-04-15 16:10:29 -0400

    Eloquent JS Chapter3 initial commit

---
+++ LearnJSDir/scripts/javascripts/Chapter3.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Sruthi on 4/14/15.
+ */
+//Chapter 3
+//Prob 1
+function min(num1,num2){
+  if (num1<num2)
+    return num1;
+  else
+    return num2;
+};
+
+console.log(min(4,3))
