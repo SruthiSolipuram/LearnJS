1c958de69a724dd1f17671fbb3079aa511e709e0
SruthiSolipuram <solipuram_sr@yahoo.com>
2015-04-18 11:39:11 -0400

    Chapter3 commit

--- LearnJSDir/scripts/javascripts/Chapter3.js
+++ LearnJSDir/scripts/javascripts/Chapter3.js
@@ -2,6 +2,7 @@
  * Created by Sruthi on 4/14/15.
  */
 //Chapter 3
+/*
 //Problem 1
 function min(num1,num2){
   if (num1<num2)
@@ -10,4 +11,31 @@ function min(num1,num2){
     return num2;
 };
 
-console.log(min(4,3))
+console.log(min(4,3))*/
+
+//Problem 2
+//function countBs (pstring){
+//  var count =0;
+//  for (var i =0; i< pstring.length;i++){
+//    if (pstring.charAt(i)==='B')
+//       count ++;
+//  };
+//  return count;
+//};
+
+function countChar (pString, pChar){
+  var count =0;
+  for (var i =0; i< pString.length;i++){
+    if (pString.charAt(i)=== pChar)
+      count ++;
+  };
+  return count;
+};
+
+function countBs (pString){
+  var count=0;
+  count = countChar(pString, "B");
+  return count;
+};
+
+console.log(countBs("tsing this B"))
